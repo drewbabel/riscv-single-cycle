fe922b46a2b335e5c621755a08038b36124e56c7
drewbabel <babeldrew@gmail.com>
2026-07-11 15:35:44 -0700

    [rtl] Add instruction memory

---
+++ rtl/imem.sv
@@ -0,0 +1,15 @@
+module imem #(
+    parameter int XLEN = 32,
+    parameter int DEPTH = 64,
+    localparam int AddrWidth = $clog2(DEPTH)
+) (
+    input  logic [XLEN-1:0] addr,
+    output logic [XLEN-1:0] instr
+);
+
+  logic [XLEN-1:0] mem[DEPTH];
+
+  // 2 bits = divide by 4
+  assign instr = mem[addr[AddrWidth+1:2]];
+
+endmodule
